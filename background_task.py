8f0cb31b014339d8ad0f74014bb26300eb5aaa4a
DarkManisalive <darkmanisalive2516@proton.me>
2025-11-16 22:05:06 +0530

    -

--- background_task.py
+++ background_task.py
@@ -1,18 +1,21 @@
 import random
+from flask import render_template
 with open("UserNameForWeb.txt", "r") as Mainfile:
     list_of_names = Mainfile.readlines()
     Mainfile = [name.strip() for name in list_of_names]
 def main():
-    Name=""
+    YourUserName=""
     for i in range(16):
-        Name += chr(random.randint(65, 90))  # Generate a random uppercase letter
+        YourUserName += chr(random.randint(65, 90))  # Generate a random uppercase letter
         if i >= 5:
-            Name += chr(random.randint(48, 57))  # Generate a random digit
-    if Name in list_of_names:
+            YourUserName += chr(random.randint(48, 57))  # Generate a random digit
+    if YourUserName in list_of_names:
         main()
     else:
         with open("UserNameForWeb.txt", "a") as Mainfile:
-            Mainfile.write(Name + "\n")
+            Mainfile.write(YourUserName + "\n")
+
+    # return render_template('StartChat.html', name=YourUserName)
 
 if __name__ == '__main__':
     main()
